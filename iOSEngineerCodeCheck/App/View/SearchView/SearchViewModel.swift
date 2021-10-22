3464f507458b38361f6110770e5afe2a7b30d66e
Akihiro Matsuyama <a@art.jp>
2021-10-22 16:07:34 +0900

    ViewModel作成

---
+++ iOSEngineerCodeCheck/App/View/SearchView/SearchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewModel.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Akihiro Matsuyama on 2021/10/22.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import Foundation
+
+class SearchViewModel: NSObject {
+
+}
