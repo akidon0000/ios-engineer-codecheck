5343942e13fc095b187544faef0c8c7c2bfbac5a
Akihiro Matsuyama <a@art.jp>
2021-10-23 18:37:36 +0900

    APIManager作成

---
+++ iOSEngineerCodeCheck/App/Model/API/APIManager.swift
@@ -0,0 +1,13 @@
+//
+//  APIManager.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Akihiro Matsuyama on 2021/10/23.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class APIManager: NSObject {
+
+}
