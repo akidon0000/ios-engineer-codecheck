7921b3c7935eefcbfa8796c11f3b663d4baf35f6
Akihiro Matsuyama <a@art.jp>
2021-10-24 20:51:22 +0900

    ApiReadMe作成

---
+++ iOSEngineerCodeCheck/App/Model/API/ApiReadMe.swift
@@ -0,0 +1,15 @@
+//
+//  ApiReadMe.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Akihiro Matsuyama on 2021/10/24.
+//  Copyright © 2021 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+
+extension ApiManager {
+    
+}
